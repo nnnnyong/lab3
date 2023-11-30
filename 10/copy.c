3bd9a946e7dce2bd0cda155d3932333a7adfa3a4
= <=>
2023-11-30 22:22:28 +0900

    .

--- 10/copy.c
+++ 10/copy.c
@@ -18,24 +18,28 @@
 char frbuffer[BUFFSIZE];
 char fwbuffer[BUFFSIZE];
 
-void shm_sem(int semid, void *shmaddr, int d);
 void p(int semid);
 void v(int semid);
 void read_shm(void *shmaddr);
 void write_shm(void *shmaddr);
+void shmwrite_sem(int semid1, int semid2, void *shmaddr);
+void shmread_sem(int semid1, int semid2, void *shmaddr);
+
+union semun {
+        int value;
+        struct semid_ds *buf;
+        unsigned short int *array;
+};
 
 int main() {
     int rfd, wfd;
-    int shmid, semid, len;
+    int shmid, semid1, semid2, len;
     void *shmaddr;
     pid_t pid;
     size_t nread;
 
-    union semun {
-        int value;
-        struct semid_ds *buf;
-        unsigned short int *array;
-    } arg;
+    union semun arg1;
+    union semun arg2;
 
     // 파일
     if ((rfd = open("read.txt", O_RDONLY)) == -1) {
@@ -48,12 +52,21 @@ int main() {
     }
 
     // 세마포어
-    if ((semid = semget(SEMKEY, 1, IPC_CREAT | 0666)) == -1) {
+    if ((semid1 = semget(0111, 1, IPC_CREAT | 0666)) == -1) {
         perror("semget failed");
         exit(1);
     }
-    arg.value = 1;
-    if (semctl(semid, 0, SETVAL, arg) == -1) {
+    if ((semid2 = semget(0112, 1, IPC_CREAT | 0666)) == -1) {
+        perror("semget failed");
+        exit(1);
+    }
+    arg1.value = 1;
+    arg2.value = 0;
+    if (semctl(semid1, 0, SETVAL, arg1) == -1) {
+        perror("semctl failed");
+        exit(1);
+    }
+    if (semctl(semid2, 0, SETVAL, arg2) == -1) {
         perror("semctl failed");
         exit(1);
     }
@@ -71,21 +84,27 @@ int main() {
     pid = fork();
     if (pid == 0) {
         while ((nread = read(rfd, frbuffer, BUFFSIZE)) > 0) {
-            shm_sem(semid, shmaddr, 1);
+            shmwrite_sem(semid1, semid2, shmaddr);
         }
+        
         exit(0);
     }
     else if (pid) {
-        while(waitpid(pid, (int *)0, WNOHANG) == 0)
-            shm_sem(semid, shmaddr, 0);
+        while(waitpid(pid, (int *)0, WNOHANG) == 0) {
+            shmread_sem(semid1, semid2, shmaddr);
+            write(wfd, fwbuffer, BUFFSIZE);
+        }
     }
     else {
         perror("fork failed");
         exit(1);
     }
 
-
-    if (semctl(semid, 0, IPC_RMID, arg) == -1) {
+    if (semctl(semid1, 0, IPC_RMID, arg1) == -1) {
+        perror("semctl failed");
+        exit(1);
+    }
+    if (semctl(semid2, 0, IPC_RMID, arg2) == -1) {
         perror("semctl failed");
         exit(1);
     }
@@ -99,24 +118,31 @@ int main() {
     }
 }
 
-void shm_sem(int semid, void *shmaddr, int d) {
-    if (d) {
-        p(semid); 
-        read_shm(shmaddr); 
-        v(semid);
+void shmwrite_sem(int semid1, int semid2, void *shmaddr) {
+    p(semid1);
+    v(semid2);
+    strcpy((char *)shmaddr, frbuffer);
+    v(semid2);
+}
+void shmread_sem(int semid1, int semid2, void *shmaddr) {
+    int i;
+
+    p(semid2);
+    if (strcmp((char *)shmaddr, "") != 0) {
+        for (i = 0; i < strlen((char *)shmaddr); i++) {
+            fwbuffer[i] = ((char *)shmaddr)[i];
+        }
+        strcpy((char *)shmaddr, "");
     }
-    else {
-        p(semid);
-        write_shm(shmaddr); 
-        v(semid);
-    }
+    v(semid2);
+    v(semid1);
 }
 
 void p(int semid) {
     struct sembuf pbuf;
     pbuf.sem_num = 0;
     pbuf.sem_op = -1;
-    pbuf.sem_flg = SEM_UNDO;
+    pbuf.sem_flg = 0;
 
     if (semop(semid, &pbuf, 1) == -1) {
         perror("semop failed");
@@ -128,7 +154,7 @@ void v(int semid) {
     struct sembuf vbuf;
     vbuf.sem_num = 0;
     vbuf.sem_op = 1;
-    vbuf.sem_flg = SEM_UNDO;
+    vbuf.sem_flg = 0;
 
     if (semop(semid, &vbuf, 1) == -1) {
         perror("semop failed");
@@ -136,15 +162,5 @@ void v(int semid) {
     }
 }
 
-void read_shm(void *shmaddr) {
-    int i;
 
-    for (i = 0; i < strlen((char *)shmaddr); i++) {
-        fwbuffer[i] = ((char *)shmaddr)[i];
-    }
-}
-
-void write_shm(void *shmaddr) {
-    strcpy((char *)shmaddr, frbuffer);
-}
 
